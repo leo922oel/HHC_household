b876559f6d4c229139e76bb97f29295222e08ca7
leo922oel <leo922oel@gmail.com>
2022-06-01 14:41:11 +0800

    update from HHC-DT1 6/01

---
+++ for_r_code/plot_box.r
@@ -0,0 +1,56 @@
+require("stringr")
+# args <- commandArgs(trailingOnly = TRUE)
+# hhstr <- as.integer(args[1])
+hhstr <- 18000
+# path <- args[2]
+
+dur_infect <- 5 # infectiousness period = 5 days
+gamma_rate <- 1 / dur_infect # recovery rate per day
+mask_eff <- 0.8 # relative risk of mask wearing policy
+
+# save_path <- paste0("/home/leo922oel/nas/", hhstr,"_new")
+save_path <- paste0("D:/user/Desktop/", hhstr,"_box")
+if (!dir.exists(save_path)) dir.create(save_path)
+
+class <- NA
+idx.peak <- NA
+Rt <- NA
+sizes <- [45000, 30000, 18000]
+for (size in sizes) {
+    path <- paste0("D/user/Desktop/", size)
+    setwd(path)
+    files <- list.files(pattern = ".csv")
+    class <- c(class, rep(as.integer(90000/45000), length(files)))
+    peak.temp <- rep(0, length(files))
+    Rt.temp <- rep(0, length(files))
+
+    for (i in 1:length(files)) {
+        file <- read.csv(files[i], header = T, sep=",")
+        mean.I <- file$mean_I
+        peak.temp[i] <- which.max(mean.I)
+        mean.hh <- file$mean_hh[-1]
+        mean.nhh <- file$mean_nhh[-1]
+        mean.total <- mean.hh + mean.nhh
+        Rt.temp[i] <- mean(mean.total[5:11])
+    }
+
+    idx.peak <- c(idx.peak, peak.temp)
+    Rt <- c(Rt, Rt.temp)
+}
+setwd(save_path)
+pngname <- "box_I.png"
+png(pngname, width = 840, height = 600, type="cairo")
+boxplot(idx.peak~class, main = "box plot of transmission peak ", xlab = "household size", ylab = "Time (Day)", ylim = c(0, 60), las = 1)
+        # abline(h=defR, lty=2, col="black", lwd = 2)
+        # abline(h=defR_hh.2, lty=2, col="red", lwd = 2)
+        # abline(h=defR_nhh, lty=2, col="green", lwd = 2)
+
+        # text(45, 5, paste0("hh contact = ", ct_hh, " | nhh contact = ", ct_nhh,
+                # "\nhh trans = ", trans_hh, " | nhh trans = ", trans_nhh), cex = 1.5, col = "blue")
+        # legend("topright", c("total", "hh", "nhh"),
+            #    fill=c("black", "red", "green"), cex=1.8)
+dev.off()
+
+pngname <- "box_Rt.png"
+png(pngname, width = 840, height = 600, type="cairo")
+boxplot(Rt~class, main = "box plot of Rt", xlab = "household size", ylab = "Rt", ylim = c(0, 8), las = 1)
